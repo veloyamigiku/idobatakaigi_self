a1cecaa76709ba2dcf98bd63862b77a3b04cec1d
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-06-05 09:59:28 +0900

    ・メッセージを表示する。

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
-
+import { List } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
+
 import { messagesRef } from "../firebase";
+import MessageItem from "./MessageItem";
 
 const useStyles = makeStyles({
   root: {
-    gridRow: 1
+    gridRow: 1,
+    overflow: 'auto',
+    width: '100%'
   }
 })
 
@@ -14,7 +18,7 @@ const MessageList = () => {
   const classes = useStyles();
 
   useEffect(() => {
-    messagesRef.orderByKey().limitToLast(3).on('value', (snapshot) => {
+    messagesRef.orderByKey().limitToLast(15).on('value', (snapshot) => {
       const messages = snapshot.val();
       if (messages == null) return; 
       const entries = Object.entries(messages);
@@ -25,7 +29,17 @@ const MessageList = () => {
       setMessages(newMessages);
     })
   }, [])
-  return <div className={classes.root}>MessageList</div>
+  return (
+    <List className={classes.root}>
+      {
+        messages.map(({ key, name, text }) => {
+          return <MessageItem key={key} name={name} text={text}>
+            item
+          </MessageItem>;
+        })
+      }
+    </List>
+  );
 
 };
 
